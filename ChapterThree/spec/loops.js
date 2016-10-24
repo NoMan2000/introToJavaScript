4216749b50f72697bbd40953b1ed7b3b0dd2b5c4
Michael Soileau <webart.video@gmail.com>
2016-10-24 08:41:54 -0500

    Initializing Bower and updating Loops
    Bower.json added to the repository.  Updating the while loops.
    
    Signed-off-by: Michael Soileau <webart.video@gmail.com>

--- ChapterThree/spec/loops.js
+++ ChapterThree/spec/loops.js
@@ -19,8 +19,36 @@ describe("Looping structures", function () {
                     expect(arr[i]).toBe(arr[2]);
                 }
             }
+
+            for (i = 0; i < arr.length; i += 1) {
+                if (i === 1) {
+                    // Continue will keep iterating over the loop, but skip the first one.
+                    continue;
+                }
+                // You will not see 1 in the output.
+                console.log(i);
+                if (i === 2) {
+                    // Break will completely exit the loop, similar to the switch statement.
+                    break;
+                }
+
+            }
+
         });
 
+        /**
+         * While loops are probably the second most common type of loop.  A while loop is easy to get wrong though since
+         * it requires you to change the afterthought manually.  A common mistake with the while loop is to leave the
+         * afterthought off and create an infinite loop.
+         */
+        it("Can run a while loop", function () {
+            var sentinelValue = 3,
+                valueToCheck = 0; // A sentinel value in programming is a value to watch for and perform an action.
+            while (valueToCheck !== sentinelValue) {
+                valueToCheck += 1;
+            }
+            expect(valueToCheck).toBe(sentinelValue);
+        });
 
     });
 });
